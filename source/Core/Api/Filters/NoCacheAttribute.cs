4372b541d2229c091e23db0782205c56d3d7b2c1
mynkow <mynkow@gmail.com>
2014-06-25 01:29:23 +0300

    NullReference check if we actually have a response. On error the response is null.

--- source/Core/Api/Filters/NoCacheAttribute.cs
+++ source/Core/Api/Filters/NoCacheAttribute.cs
@@ -1,18 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Http.Filters;
 
 namespace Thinktecture.IdentityManager.Core.Api.Filters
 {
-    class NoCacheAttribute : ActionFilterAttribute
+    public class NoCacheAttribute : ActionFilterAttribute
     {
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             base.OnActionExecuted(actionExecutedContext);
-            if (actionExecutedContext.Response.IsSuccessStatusCode)
+            if (actionExecutedContext.Response != null && actionExecutedContext.Response.IsSuccessStatusCode)
             {
                 var cc = new System.Net.Http.Headers.CacheControlHeaderValue();
                 cc.NoStore = true;
